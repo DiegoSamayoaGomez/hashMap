2ddd70cab6db490a9779d595763a88f6b64def31
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-06-12 23:34:27 -0600

    Store a set of key value pairs in the bucket

--- hashMap.js
+++ hashMap.js
@@ -1,6 +1,8 @@
 const HashMap = () => {
-  let loadFactor;
-  let capacity;
+  let myBucket = [0];
+  let numberOfBuckets = myBucket.length;
+  let loadFactor = 0.75;
+  let capacity = numberOfBuckets;
 
   // Takes a key and produces a hash code with it.
   // ** EDGE CASE ** For long keys, the hash code will exceed the integer value of JS
@@ -13,7 +15,7 @@ const HashMap = () => {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
 
-    return hashCode;
+    return hashCode % 16;
   };
 
   /*
@@ -21,7 +23,33 @@ const HashMap = () => {
    value that is assigned to this key. 
    If a key already exists, then the old value is overwritten
   */
-  const set = (key, value) => {};
+  const set = (key, value) => {
+    // Pass the key to the hash function
+    let bucketLocation = hash(key);
+    // Find the bucket at index
+    // Store the kay value pair in that bucket
+    myBucket.splice(bucketLocation, 0, { [key]: value });
+    console.log("Created, I guess");
+    console.log(myBucket);
+  };
 
-  return { hash };
+  // Get the array of values stored in the bucket
+  const getBucket = () => myBucket;
+  const getNumberOfBuckets = () => {
+    myBucket.length;
+  };
+
+  return {
+    hash, // Delete later access to this function
+    getNumberOfBuckets, // Delete later this function entirely
+    set,
+    getBucket,
+  };
 };
+
+let instanceOfHashMap = HashMap();
+// Know the index of the key value pair
+console.log(instanceOfHashMap.hash("Diego"));
+instanceOfHashMap.set("Diego", "Samayoa"); // Delete later the access to this function
+console.log("Print bucket", instanceOfHashMap.getBucket());
+console.log(instanceOfHashMap.getNumberOfBuckets());
